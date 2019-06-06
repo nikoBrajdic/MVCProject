2e61fbd7d732a06cdc7f52689e6fda1ca73b9a89
Niko Brajdić <brajdic.niko@gmail.com>
2019-06-06 23:48:02 +0200

    some property rearranging

--- TKDLocalWebClient.Model/Poomsae.cs
+++ TKDLocalWebClient.Model/Poomsae.cs
@@ -8,9 +8,9 @@ namespace TKDLocalWebClient.Model
         [Key]
         public int ID { get; set; }
         public string Name { get; set; }
-        public string Type { get; set; }
         public string ShortName { get; set; }
         public string Ordinal { get; set; }
+        public string Type { get; set; }
         public string Path { get; set; }
     }
 }
